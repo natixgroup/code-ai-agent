61e93af0eb182e11340e80e039ac49c3e868d139
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2024-07-31 22:38:34 +0300

    Send to debug

--- code/src/ai-http-client.ts
+++ code/src/ai-http-client.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default class AIHttpClient {
   private provider: string;
   private url: string;
+  private debugUrl: string;
   private body: object;
   private token: string;
   private tokenHeaderName: string;
@@ -12,19 +13,20 @@ export default class AIHttpClient {
     this.provider = provider;
     this.body = {};
     this.url = '';
+    this.debugUrl = '';
     this.token = '';
     this.tokenHeaderName = '';
     this.tokenHeaderValue = '';
     if (this.provider === 'chatgpt') {
       this.url = 'https://api.openai.com/v1/chat/completions';
-      // this.url = 'https://eowloffrpvxwtqp.m.pipedream.net/v1/chat/completions'
+      this.debugUrl = 'https://eowloffrpvxwtqp.m.pipedream.net/v1/chat/completions'
       this.token = process.env.OPENAI_API_KEY? process.env.OPENAI_API_KEY : '';
       this.tokenHeaderName = 'Authorization';
       this.tokenHeaderValue = `Bearer ${this.token}`;
     } 
     if (this.provider === 'gemini') {
       this.url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-latest:generateContent'
-      // this.url = 'https://eowloffrpvxwtqp.m.pipedream.net/v1beta/models/gemini-1.5-pro-latest:generateContent'
+      this.debugUrl = 'https://eowloffrpvxwtqp.m.pipedream.net/v1beta/models/gemini-1.5-pro-latest:generateContent'
       this.token = process.env.GEMINI_API_KEY ? process.env.GEMINI_API_KEY : '';
       this.tokenHeaderName = 'x-goog-api-key';
       this.tokenHeaderValue = this.token;
@@ -38,6 +40,7 @@ export default class AIHttpClient {
   async post(): Promise<any> {
     try {
       axios.defaults.headers.common[this.tokenHeaderName] = this.tokenHeaderValue;
+      axios.post(this.debugUrl, this.body);
       const response = await axios.post(this.url, this.body);
       console.log(response.data);
       return response.data;
