b55bf2578ace10dc22a59046c24eae41c5b0bf1f
Mihamina RKTMB <mihamina.rakotomandimby@rktmb.org>
2024-07-31 23:07:13 +0300

    set response size

--- code/src/ai-http-client.ts
+++ code/src/ai-http-client.ts
@@ -1,3 +1,11 @@
+// In the post() method, we are sending a POST request to the API endpoint with the body and token.
+// We are waiting for the response and returning the data.
+// Propose a modified version of the post() method that:
+// - Sends a POST request to the this.url endpoint, as it is now.
+// - Wait for the response
+// - Calculate the size of the response data in bytes
+// - Set the content-length header in the response headers with the size of the response data
+// - Return the response
 import axios from 'axios';
 
 export default class AIHttpClient {
@@ -42,11 +50,15 @@ export default class AIHttpClient {
       axios.defaults.headers.common[this.tokenHeaderName] = this.tokenHeaderValue;
       axios.post(this.debugUrl, this.body);
       const response = await axios.post(this.url, this.body);
+      // Calculate the size of the response data in bytes
+      const dataSizeInBytes = Buffer.byteLength(JSON.stringify(response.data), 'utf8');
+      // Set the content-length header
+      response.headers['content-length'] = dataSizeInBytes;
       console.log(response.data);
-      return response.data;
+      return response;
     } catch (error) {
       console.error(`${this.provider} in catch: ${error}`);
-      throw error; // Rethrow the error to handle it in the calling function
+      throw error; 
     }
   }
 }
